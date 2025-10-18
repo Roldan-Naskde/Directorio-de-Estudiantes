4059370d94222af8f2fd67c8d7f85a9e8f5ee56b
Roldan-Naskde <roldan.melendez90@gmail.com>
2025-10-18 18:42:02 -0500

    creacion de l backend

---
+++ backend/src/models/student.model.js
@@ -0,0 +1,9 @@
+// Este archivo sirve como referencia del esquema lógico
+export const Student = {
+  id: Number,
+  firstName: String,
+  lastName: String,
+  email: String,
+  enrollmentDate: Date,
+  active: Boolean
+};
